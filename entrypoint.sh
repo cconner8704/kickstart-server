94d62d5b74eeadc5cfcdd029645ad0ebc7b6d5e4
Chris Conner <cconner@cloudera.com>
2018-04-08 11:08:29 -0400

    Still in development

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
+
+/bin/cp -fr /usr/share/syslinux/* /var/lib/tftpboot
+
 while [ 1 ]
 do
   sleep 1000
 done
 
-/bin/cp -fr /usr/share/syslinux/* /var/lib/tftpboot
 systemctl start dnsmasq
 systemctl start vsftpd
